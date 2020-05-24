40c9aa32bb8b0171fdb4859b551de824268c1d10
Rodolfo Villas Boas <rodolfo.boas@7comm.com.br>
2020-05-23 21:04:51 -0300

    initial commit

---
+++ precache-manifest.909637cd11d6d706ba83e6e9bd328d7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d87f12388b55db0f512d08b4b3771c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4787329f3b74bec9ece",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7a8f922aa18e06c02c8e",
+    "url": "/static/js/2.6ee02b15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6ee02b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4787329f3b74bec9ece",
+    "url": "/static/js/main.6b35a93f.chunk.js"
+  },
+  {
+    "revision": "8168fb5ab593e12ceb01",
+    "url": "/static/js/runtime-main.5a915bea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
