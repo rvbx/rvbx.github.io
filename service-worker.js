832d729ef475e8f9f61f6aac0b32c009a52b9aba
rvbx <rodolfo.boas@outlook.com>
2020-08-01 23:39:58 -0300

    update files

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa3fafc03923114527de47ac10851860.js"
+  "/precache-manifest.69a09b4f6bbe11fd221c46a5169bb74b.js"
 );
 
 self.addEventListener('message', (event) => {
